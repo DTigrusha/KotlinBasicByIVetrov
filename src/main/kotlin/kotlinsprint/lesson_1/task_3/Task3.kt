bfaeac9753fc6f867f001275eff71fae9162e55d
DTigrusha <DTigrusha@users.noreply.github.com>
2023-05-10 11:31:07 +0300

    KS-1-3 create new variables and print data

---
+++ src/main/kotlin/kotlinsprint/lesson_1/task_3/Task3.kt
@@ -0,0 +1,15 @@
+package kotlinsprint.lesson_1.task_3
+
+fun main(args: Array<String>) {
+    val year = 1961
+    var hour = 9
+    var minute = 7
+    println("$year")
+    println("$hour")
+    println("$minute")
+
+    hour = 10
+    minute = 55
+    println("$hour:$minute")
+}
+
