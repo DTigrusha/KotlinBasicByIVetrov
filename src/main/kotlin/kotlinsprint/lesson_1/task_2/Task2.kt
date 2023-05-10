c697e4d89b6627cd8e90ba304b703d2834d53562
DTigrusha <DTigrusha@users.noreply.github.com>
2023-05-10 11:04:36 +0300

    KS-1-2 print data from Task1, create new variable and print data

--- src/main/kotlin/kotlinsprint/lesson_1/task_2/Task2.kt
+++ src/main/kotlin/kotlinsprint/lesson_1/task_2/Task2.kt
@@ -4,15 +4,12 @@ fun main(args: Array<String>) {
     val numberOfOrders: Int = 75
     val thanksForPurchase: String =
         "Уважаемый покупатель! Благодарим Вас за покупку в нашем интернет-магазине! Ждем Вас снова!"
-
     println("$numberOfOrders")
     println(thanksForPurchase)
 
     var numberOfEmployees: Int = 2000
-
 //    println("$numberOfEmployees")
 
     numberOfEmployees = 1999
-
     println("$numberOfEmployees")
 }
