62240310cc014f93559c29ad8663e2b88758789b
DTigrusha <DTigrusha@users.noreply.github.com>
2023-05-10 15:27:42 +0300

    KS-2-3 count time of arrival

---
+++ src/main/kotlin/kotlinsptint/lesson_2/task_3/Task_3.kt
@@ -0,0 +1,15 @@
+package kotlinsptint.lesson_2.task_3
+
+fun main() {
+    val hourOfDeparture = 9
+    val minuteOfDeparture = 39
+    val travelTime = 457
+    var hourOfArrival = (travelTime / 60) + hourOfDeparture
+    var minuteOfArrival = (travelTime % 60) + minuteOfDeparture
+    if (minuteOfArrival > 60) {
+        hourOfArrival += minuteOfArrival / 60
+        minuteOfArrival %= 60
+    }
+
+    println("$hourOfArrival:$minuteOfArrival")
+}
