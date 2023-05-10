62a083a73bf9fe567d4cf2637507ff4af4272c87
DTigrusha <DTigrusha@users.noreply.github.com>
2023-05-10 11:01:06 +0300

    KS-1-2 print data and create new variable

---
+++ src/main/kotlin/kotlinsprint/lesson_1/task_2/Task2.kt
@@ -0,0 +1,18 @@
+package kotlinsprint.lesson_1.task_2
+
+fun main(args: Array<String>) {
+    val numberOfOrders: Int = 75
+    val thanksForPurchase: String =
+        "Уважаемый покупатель! Благодарим Вас за покупку в нашем интернет-магазине! Ждем Вас снова!"
+
+    println("$numberOfOrders")
+    println(thanksForPurchase)
+
+    var numberOfEmployees: Int = 2000
+
+//    println("$numberOfEmployees")
+
+    numberOfEmployees = 1999
+
+    println("$numberOfEmployees")
+}
