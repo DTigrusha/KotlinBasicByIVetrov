c7b06f9a1bb4f6e05381424a5522c6a77e193df3
DTigrusha <DTigrusha@users.noreply.github.com>
2023-05-10 11:49:48 +0300

    KS-1-4 create new variables and print data

---
+++ src/main/kotlin/kotlinsprint/lesson_1/task_4/Task4.kt
@@ -0,0 +1,16 @@
+package kotlinsprint.lesson_1.task_4
+
+fun main(args: Array<String>) {
+   val length: Long = 40868600000
+   val age: Byte = 27
+   val partOfDay: Float = 0.075F
+   val seconds: Short = 6480
+   val partOfYears: Double = 2.0547945205479453E-4
+   val apogee: Int = 327000
+   println("$length")
+   println("$age")
+   println("$partOfDay")
+   println("$seconds")
+   println("$partOfYears")
+   println("$apogee")
+}
