c0c9623655e8c445fa4138977bac041f0e63e464
DTigrusha <DTigrusha@users.noreply.github.com>
2023-05-11 09:43:02 +0300

    KS-2-3 fix bugs with calculation system

--- src/main/kotlin/kotlinsptint/lesson_2/task_3/Task_3.kt
+++ src/main/kotlin/kotlinsptint/lesson_2/task_3/Task_3.kt
@@ -3,13 +3,11 @@ package kotlinsptint.lesson_2.task_3
 fun main() {
     val hourOfDeparture = 9
     val minuteOfDeparture = 39
-    val travelTime = 457
-    var hourOfArrival = (travelTime / 60) + hourOfDeparture
-    var minuteOfArrival = (travelTime % 60) + minuteOfDeparture
-    if (minuteOfArrival >= 60) {
-        hourOfArrival += minuteOfArrival / 60
-        minuteOfArrival %= 60
-    }
+    val totalMinuteOfDeparture = hourOfDeparture * 60 + minuteOfDeparture
+    val minuteOfTravelTime = 457
+    val totalMinuteOfArrival = totalMinuteOfDeparture + minuteOfTravelTime
+    val hourOfArrival = totalMinuteOfArrival / 60
+    val minuteOfArrival = totalMinuteOfArrival % 60
 
     println("$hourOfArrival:$minuteOfArrival")
 }
