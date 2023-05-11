25057a89571b7ac264bc48cfd3375d8c8cf4d5c6
DTigrusha <DTigrusha@users.noreply.github.com>
2023-05-11 09:27:41 +0300

    KS-1-5 fix bugs with execution

--- src/main/kotlin/kotlinsprint/lesson_1/task_5/Task5.kt
+++ src/main/kotlin/kotlinsprint/lesson_1/task_5/Task5.kt
@@ -1,8 +1,9 @@
 package kotlinsprint.lesson_1.task_5
 
-fun main(args: Array<String>) {
+fun main() {
     val totalSeconds = 6480
-    val minutes = totalSeconds/60
-    println("$minutes:${"%02d".format(totalSeconds%60)}")
+    val minutes = totalSeconds / 60
+
+    println("$minutes:${"%02d".format(totalSeconds % 60)}")
 
 }
