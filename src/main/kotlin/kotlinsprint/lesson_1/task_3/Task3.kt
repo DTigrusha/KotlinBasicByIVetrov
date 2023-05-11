b9de89e58cba4472ef4dcb2380dd9ee2e9e45795
DTigrusha <DTigrusha@users.noreply.github.com>
2023-05-11 09:03:36 +0300

    KS-1-3 fix bugs with variables

--- src/main/kotlin/kotlinsprint/lesson_1/task_3/Task3.kt
+++ src/main/kotlin/kotlinsprint/lesson_1/task_3/Task3.kt
@@ -1,15 +1,15 @@
 package kotlinsprint.lesson_1.task_3
 
-fun main(args: Array<String>) {
+fun main() {
     val year = 1961
-    var hour = 9
-    var minute = 7
+    var hour = "09"
+    var minute = "07"
     println("$year")
-    println("$hour")
-    println("$minute")
+    println(hour)
+    println(minute)
 
-    hour = 10
-    minute = 55
+    hour = "10"
+    minute = "55"
     println("$hour:$minute")
 }
 
