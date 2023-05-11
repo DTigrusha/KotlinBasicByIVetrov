a6a399a5fb521c44a8df30316d2976fc2774c690
DTigrusha <DTigrusha@users.noreply.github.com>
2023-05-11 14:31:11 +0300

    KS-4-1 print info about availibility of tables in restaurants

---
+++ src/main/kotlin/kotlinsprint/lesson_4/task_1/Task_1.kt
@@ -0,0 +1,9 @@
+package kotlinsprint.lesson_4.task_1
+
+fun main() {
+    val numberOfTables = 13
+    val todayReserved = 13
+    val tomorrowReserved = 9
+
+    println("Доступность столиков на сегодня: ${(numberOfTables - todayReserved) > 0}\nДоступность столиков на завтра: ${(numberOfTables - tomorrowReserved) > 0}")
+}
