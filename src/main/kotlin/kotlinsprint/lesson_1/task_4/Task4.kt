4e737704e947237fd8c24dd733bbbdeb6ccbe74e
DTigrusha <DTigrusha@users.noreply.github.com>
2023-05-11 09:09:36 +0300

    KS-1-4 fix bugs with variables

--- src/main/kotlin/kotlinsprint/lesson_1/task_4/Task4.kt
+++ src/main/kotlin/kotlinsprint/lesson_1/task_4/Task4.kt
@@ -1,16 +1,17 @@
 package kotlinsprint.lesson_1.task_4
 
-fun main(args: Array<String>) {
+fun main() {
    val length: Long = 40868600000
    val age: Byte = 27
    val partOfDay: Float = 0.075F
    val seconds: Short = 6480
    val partOfYears: Double = 2.0547945205479453E-4
    val apogee: Int = 327000
-   println("$length")
-   println("$age")
-   println("$partOfDay")
-   println("$seconds")
-   println("$partOfYears")
-   println("$apogee")
+
+   println(length)
+   println(age)
+   println(partOfDay)
+   println(seconds)
+   println(partOfYears)
+   println(apogee)
 }
