fa30fe37317120ede893acad3eaae9ce5acdcac7
DTigrusha <DTigrusha@users.noreply.github.com>
2023-05-12 14:56:55 +0300

    KS-4-2 create constants and delete empty lines

--- src/main/kotlin/kotlinsprint/lesson_4/task_2/Task_2.kt
+++ src/main/kotlin/kotlinsprint/lesson_4/task_2/Task_2.kt
@@ -1,19 +1,17 @@
 package kotlinsprint.lesson_4.task_2
 
 fun main() {
-    val weightLowLimit = 35
-    val weightHighLimit = 100
-    val volumeHighLimit = 120
     val cargoWeight1 = 20
     val cargoVolume1 = 80
     val cargoWeight2 = 50
     val cargoVolume2 = 100
 
-    println("Average для груза весом $cargoWeight1 кг и объемом $cargoVolume1: ${(cargoWeight1 >= weightLowLimit &&
-            cargoWeight1 <= weightHighLimit) && (cargoVolume1 < volumeHighLimit)}\nAverage для груза весом $cargoWeight2 " +
-            "кг и объемом $cargoVolume2: ${(cargoWeight2 >= weightLowLimit && cargoWeight2 <= weightHighLimit)
-                    && (cargoVolume2 < volumeHighLimit)}")
-
-
-
+    println("Average для груза весом $cargoWeight1 кг и объемом $cargoVolume1: ${(cargoWeight1 >= WEIGHT_LOW_LIMIT &&
+            cargoWeight1 <= WEIGHT_HIGH_LIMIT) && (cargoVolume1 < VOLUME_HIGH_LIMIT)}\nAverage для груза весом " +
+            "$cargoWeight2 кг и объемом $cargoVolume2: ${(cargoWeight2 >= WEIGHT_LOW_LIMIT &&
+                    cargoWeight2 <= WEIGHT_HIGH_LIMIT) && (cargoVolume2 < VOLUME_HIGH_LIMIT)}")
 }
+
+const val WEIGHT_LOW_LIMIT = 35
+const val WEIGHT_HIGH_LIMIT = 100
+const val VOLUME_HIGH_LIMIT = 120
