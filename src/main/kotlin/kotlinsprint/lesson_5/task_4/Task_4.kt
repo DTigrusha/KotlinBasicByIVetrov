ea53dcf9058efbe99afa7112db8106a611b8d9b0
DTigrusha <DTigrusha@users.noreply.github.com>
2023-05-15 10:00:32 +0300

    KS-5-4 apply an AutoFormat and add result variable

--- src/main/kotlin/kotlinsprint/lesson_5/task_4/Task_4.kt
+++ src/main/kotlin/kotlinsprint/lesson_5/task_4/Task_4.kt
@@ -7,12 +7,17 @@ fun main() {
     println("Для входа в приложение необходима авторизация.\nВведите, пожалуйста, Ваш логин:")
 
     val inputLogin = readLine()
+    var systemAnswer =
+        if (inputLogin != userLogin) "Пользователь с таким именем не зарегистрирован в приложении. " +
+                "Для входа в приложение пройдите регистрацию."
+        else "Введите, пожалуйста, Ваш пароль:"
 
-    println(if (inputLogin != userLogin) "Пользователь с таким именем не зарегистрирован в приложении. " +
-            "Для входа в приложение пройдите регистрацию." else "Введите, пожалуйста, Ваш пароль:")
+    println(systemAnswer)
 
     val inputPassword = readLine()
+    systemAnswer =
+        if (inputPassword == userPassword) "Добро пожаловать, $userLogin!\nВы успешно авторизованы в приложении."
+        else "Ошибка авторизации."
 
-    println(if (inputPassword == userPassword) "Добро пожаловать, $userLogin!\nВы успешно авторизованы в приложении."
-    else "Ошибка авторизации.")
+    println(systemAnswer)
 }
