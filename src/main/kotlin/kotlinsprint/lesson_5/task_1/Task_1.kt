b25136ec49e8bf8f7acd7811f232e3ec40ef6dbe
DTigrusha <DTigrusha@users.noreply.github.com>
2023-05-15 09:16:06 +0300

    KS-5-1 add variables and change print function

--- src/main/kotlin/kotlinsprint/lesson_5/task_1/Task_1.kt
+++ src/main/kotlin/kotlinsprint/lesson_5/task_1/Task_1.kt
@@ -1,11 +1,13 @@
 package kotlinsprint.lesson_5.task_1
 
 fun main() {
-    println("Для входа в приложение Вам необходимо пройти проверку!\nПожалуйста, решите пример: \"2 + 5\" и введите " +
-            "Ваш ответ ниже:")
+    val firstCheckNumber = 2
+    val secondCheckNumber = 5
+    println("Для входа в приложение Вам необходимо пройти проверку!\nПожалуйста, решите пример: " +
+            "\"$firstCheckNumber + $secondCheckNumber\" и введите Ваш ответ ниже:")
 
-    val inputAnswer = readLine()!!.toInt()
-    if (inputAnswer == (2 + 5)) println("Добро пожаловать!")
+    val inputAnswer = readln().toInt()
+    if (inputAnswer == (firstCheckNumber + secondCheckNumber)) println("Добро пожаловать!")
     else println("Доступ запрещен.")
 
 }
