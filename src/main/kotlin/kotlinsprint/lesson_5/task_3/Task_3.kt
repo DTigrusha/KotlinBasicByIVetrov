2bcf35b63b59c78bf089b06bd1818815a6aec6da
DTigrusha <DTigrusha@users.noreply.github.com>
2023-05-15 09:36:50 +0300

    KS-5-3 change print function

--- src/main/kotlin/kotlinsprint/lesson_5/task_3/Task_3.kt
+++ src/main/kotlin/kotlinsprint/lesson_5/task_3/Task_3.kt
@@ -7,17 +7,17 @@ fun main() {
     println("Добро пожаловать в ИГРУ! В нашей ИГРЕ Вам необходимо угадать два загаданных компьютером числа " +
             "от 1 до 100!\nВвведите, пожалуйста, первое число:")
 
-    val userFirstNumber = readLine()!!.toInt()
+    val userFirstNumber = readln().toInt()
 
     println("Ввведите, пожалуйста, второе число:")
 
-    val userSecondNumber = readLine()!!.toInt()
+    val userSecondNumber = readln().toInt()
 
     when {
-        userFirstNumber == firstNumber && userSecondNumber == secondNumber -> println("Поздравляем! Вы выиграли " +
-                "главный приз!")
-        userFirstNumber == firstNumber || userSecondNumber == secondNumber -> println("Вы выиграли утешительный " +
-                "приз!")
+        userFirstNumber == firstNumber && userSecondNumber == secondNumber ->
+            println("Поздравляем! Вы выиграли главный приз!")
+        userFirstNumber == firstNumber || userSecondNumber == secondNumber ->
+            println("Вы выиграли утешительный приз!")
         else -> println("Неудача! Крутите барабан!")
     }
     println("Комьютер загадал числа: $firstNumber и $secondNumber.")
