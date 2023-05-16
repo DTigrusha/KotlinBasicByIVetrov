498ffd46d431c12a39d1f5f250a973fe4ec63a2b
DTigrusha <DTigrusha@users.noreply.github.com>
2023-05-16 14:43:27 +0300

    KS-5-3 apply AutoFormat

--- src/main/kotlin/kotlinsprint/lesson_5/task_3/Task_3.kt
+++ src/main/kotlin/kotlinsprint/lesson_5/task_3/Task_3.kt
@@ -4,8 +4,10 @@ fun main() {
     val firstNumber = 20
     val secondNumber = 50
 
-    println("Добро пожаловать в ИГРУ! В нашей ИГРЕ Вам необходимо угадать два загаданных компьютером числа " +
-            "от 1 до 100!\nВвведите, пожалуйста, первое число:")
+    println(
+        "Добро пожаловать в ИГРУ! В нашей ИГРЕ Вам необходимо угадать два загаданных компьютером числа от 1 до 100!" +
+                "\nВвведите, пожалуйста, первое число:"
+    )
 
     val userFirstNumber = readln().toInt()
 
@@ -16,8 +18,10 @@ fun main() {
     when {
         userFirstNumber == firstNumber && userSecondNumber == secondNumber ->
             println("Поздравляем! Вы выиграли главный приз!")
+
         userFirstNumber == firstNumber || userSecondNumber == secondNumber ->
             println("Вы выиграли утешительный приз!")
+
         else -> println("Неудача! Крутите барабан!")
     }
     println("Комьютер загадал числа: $firstNumber и $secondNumber.")
