d4165ea47a37ab8fcc89189302eabdcec8359892
DTigrusha <DTigrusha@users.noreply.github.com>
2023-05-16 15:01:58 +0300

    KS-8-1 count week views of advertising

---
+++ src/main/kotlin/kotlinsprint/lesson_8/task_1/Task_1.kt
@@ -0,0 +1,22 @@
+package kotlinsprint.lesson_8.task_1
+
+fun main() {
+    val mondayViews = 7
+    val tuesdayViews = 17
+    val wednesdayViews = 23
+    val thursdayViews = 43
+    val fridayViews = 12
+    val saturdayViews = 4
+    val sundayViews = 2
+    var sumOfViews = 0
+    val weekViews = arrayOf(
+        mondayViews, tuesdayViews, wednesdayViews, thursdayViews, fridayViews, saturdayViews,
+        sundayViews
+    )
+
+    for (i in weekViews.indices) {
+        sumOfViews += weekViews[i]
+    }
+
+    println(sumOfViews)
+}
