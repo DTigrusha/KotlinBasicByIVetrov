8f5f16aa46b0e610b547c2da74f51039f34f4c50
DTigrusha <DTigrusha@users.noreply.github.com>
2023-05-16 15:49:15 +0300

    KS-4-5 format code

--- src/main/kotlin/kotlinsprint/lesson_4/task_5/Task_5.kt
+++ src/main/kotlin/kotlinsprint/lesson_4/task_5/Task_5.kt
@@ -6,26 +6,21 @@ fun main() {
     val provision = 50
 
     println("Укажите, есть ли повреждения корпуса корабля: true/false?")
-
     val inputDamage = readln().toBoolean()
 
     println("Укажите текущую численность экипажа:")
-
     val inputSizeOfCrew = readln().toInt()
 
     println("Укажите количество ящиков проовизии:")
-
     val inputProvision = readln().toInt()
 
     println("Укажите, благоприятны ли метеоусловия: true/false?")
-
     val inputWeatherConditions = readln().toBoolean()
 
-    println(if ((!inputDamage && (inputSizeOfCrew in minSizeOfCrew until recommendedSizeOfCrew)
-        && inputProvision > provision && (inputWeatherConditions || !inputWeatherConditions)) || (inputDamage
-                && inputSizeOfCrew == recommendedSizeOfCrew && inputProvision > provision
-                && inputWeatherConditions)) "Все условия соблюдены, корабль может отправляться в рейс."
-    else "Условия отправки в рейс не соблюдены. Корабль отплыть не может.")
-
-
+    if ((!inputDamage && (inputSizeOfCrew in minSizeOfCrew until recommendedSizeOfCrew) &&
+                inputProvision > provision && (inputWeatherConditions || !inputWeatherConditions)) ||
+        (inputDamage && inputSizeOfCrew == recommendedSizeOfCrew && inputProvision > provision &&
+                inputWeatherConditions)
+    ) println("Все условия соблюдены, корабль может отправляться в рейс.")
+    else println("Условия отправки в рейс не соблюдены. Корабль отплыть не может.")
 }
