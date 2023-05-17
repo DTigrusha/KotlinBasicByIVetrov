3d2242e6f4c40c0b03bb80ce6f1b1a50bec2400c
DTigrusha <DTigrusha@users.noreply.github.com>
2023-05-17 09:34:31 +0300

    KS-6-1 delete empty strings and semicolon

--- src/main/kotlin/kotlinsprint/lesson_6/task_1/Task_1.kt
+++ src/main/kotlin/kotlinsprint/lesson_6/task_1/Task_1.kt
@@ -2,28 +2,22 @@ package kotlinsprint.lesson_6.task_1
 
 fun main() {
     println("Здравствуйте!\nДля регистрации в приложении создайте сначала логин:")
-
-    val registeredLogin = readln();
+    val registeredLogin = readln()
 
     println("Теперь создайте пароль:")
-
     val registeredPassword = readln()
 
     println("Для авторизации в приложении введите Ваш логин:")
-
     var inputLogin = readln()
 
     println("Теперь введите Ваш пароль:")
-
     var inputPassword = readln()
 
     while (inputPassword != registeredPassword) {
         println("Вы ввели не тот пароль!\nДля авторизации в приложении введите заново Ваш логин:")
-
         inputLogin = readln()
 
         println("Теперь введите Ваш пароль:")
-
         inputPassword = readln()
     }
 
