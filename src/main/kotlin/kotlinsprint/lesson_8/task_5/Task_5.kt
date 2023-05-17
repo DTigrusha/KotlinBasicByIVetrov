c7f3c708ca1a3d517cad406ecd25df806a988a5e
DTigrusha <DTigrusha@users.noreply.github.com>
2023-05-17 10:45:45 +0300

    KS-8-4 create array of ingredients

---
+++ src/main/kotlin/kotlinsprint/lesson_8/task_5/Task_5.kt
@@ -0,0 +1,15 @@
+package kotlinsprint.lesson_8.task_5
+
+fun main() {
+    println("Введите количество ингредиентов, которые вы хотите добавить в список:")
+    val numberOfIngredients = readln().toInt()
+
+    val ingredients = arrayListOf<String>()
+
+    for (i in 0 until numberOfIngredients) {
+        println("Введите название ингредиента №${i + 1}:")
+        val ingredient = readln()
+        ingredients.add(ingredient)
+    }
+    val arrayOfIngredients = ingredients.toTypedArray()
+}
