ba20c3eab18db579ffdaec87380ccfbf75370c94
DTigrusha <DTigrusha@users.noreply.github.com>
2023-05-20 11:30:02 +0300

    KS-11-1 merge two files

--- src/main/kotlin/kotlinsprint/lesson_11/task_1/User.kt
+++ src/main/kotlin/kotlinsprint/lesson_11/task_1/User.kt
@@ -10,3 +10,28 @@ class User(
         println("id: $id,\nlogin: $login,\npassword: $password,\nemail: $email")
     }
 }
+
+fun main() {
+
+    val user1 = User(
+        id = 1,
+        login = "морковь",
+        password = "qwerty",
+        email = "qwerty@mail.ru"
+    )
+
+    val user2 = User(
+        id = 2,
+        login = "свекла",
+        password = "ytrewq",
+        email = "ytrewq@mail.ru"
+    )
+
+    println("Информация о пользователе user1:")
+    user1.printUserData()
+
+    println()
+
+    println("Информация о пользователе user2:")
+    user2.printUserData()
+}
