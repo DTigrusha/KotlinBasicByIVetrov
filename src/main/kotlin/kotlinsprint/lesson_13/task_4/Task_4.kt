4ee931c52b9b9483bbd13dd5788c6589eac9ecff
DTigrusha <DTigrusha@users.noreply.github.com>
2023-05-23 15:16:21 +0300

    KS-13-4 change "" to null

--- src/main/kotlin/kotlinsprint/lesson_13/task_4/Task_4.kt
+++ src/main/kotlin/kotlinsprint/lesson_13/task_4/Task_4.kt
@@ -3,7 +3,7 @@ package kotlinsprint.lesson_13.task_4
 class NewContact(val name: String?, val mobileNumber: Long?, val company: String?) {
 
     fun printContact() {
-        println("Имя: ${name ?: "null"}\nНомер: $mobileNumber\nКомпания: ${company ?: "null"}\n")
+        println("Имя: $name\nНомер: $mobileNumber\nКомпания: $company\n")
     }
 
 }
@@ -13,22 +13,28 @@ fun addNewContact(): MutableList<NewContact> {
 
     do {
         println("Для добавление нового контакта в телефонную книгу введите имя контакта:")
-        val inputName: String = readln()
+        var inputName: String? = readln()
+        if (inputName!!.isEmpty()) {
+            inputName = null
+        }
 
         println("Введите номер мобильного телефона контакта:")
         val inputMobileNumber = readln().toLongOrNull()
 
         println("Укажите место работы контакта:")
-        val inputCompany: String = readln()
+        var inputCompany: String? = readln()
+        if (inputCompany!!.isEmpty()) {
+            inputCompany = null
+        }
 
         val newContact = NewContact(inputName, inputMobileNumber, inputCompany)
 
         println("Если хотите добавить новую запись, введите: \"да\":")
         val answer = readln().lowercase()
 
-        if ((inputMobileNumber != null && inputName != "" && inputCompany != "") ||
-            (inputMobileNumber != null && inputName != "" && inputCompany == "") ||
-            (inputMobileNumber != null && inputName == "" && inputCompany != "")
+        if ((inputMobileNumber != null && inputName != null && inputCompany != null) ||
+            (inputMobileNumber != null && inputName != null && inputCompany == null) ||
+            (inputMobileNumber != null && inputName == null && inputCompany != null)
         ) {
             listOfContacts.add(newContact)
         }
