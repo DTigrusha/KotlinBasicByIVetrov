dd3153045e6f507dfa9c779cf7183d465e46c93e
DTigrusha <DTigrusha@users.noreply.github.com>
2023-05-23 15:25:35 +0300

    KS-12-3 move parenthesis, change isRainy type

--- src/main/kotlin/kotlinsprint/lesson_12/task_3/Task_3.kt
+++ src/main/kotlin/kotlinsprint/lesson_12/task_3/Task_3.kt
@@ -3,30 +3,31 @@ package kotlinsprint.lesson_12.task_3
 class Day(
     val dayTemperature: Int,
     val nightTemperature: Int,
-    val isRainy: String = "да",
-    val atmosphericPressure: Int,) {
+    val isRainy: Boolean = true,
+    val atmosphericPressure: Int,
+) {
 
     fun printWeatherInfo() {
         println(
             "Информация о погоде:\nдневная температура - $dayTemperature,\nночная температура - $nightTemperature," +
-                    "\nшел ли дождь - $isRainy,\nатмосферное давление - $atmosphericPressure"
+                    "\nшел ли дождь - ${if(isRainy) "да" else "нет"},\nатмосферное давление - $atmosphericPressure"
         )
     }
 }
 
 fun main() {
 
-    val day1 = Day(25, 20, "нет", 765)
+    val day1 = Day(25, 20, false, 765)
     day1.printWeatherInfo()
 
     println()
 
-    val day2 = Day(10, 3, "да", 750)
+    val day2 = Day(10, 3, true, 750)
     day2.printWeatherInfo()
 
     println()
 
-    val day3 = Day(35, 33, "нет", 765)
+    val day3 = Day(35, 33, false, 765)
     day3.printWeatherInfo()
 
 }
