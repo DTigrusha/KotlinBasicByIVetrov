98d75902d630ecbe942fe9bdc5b614388aeaf0cc
DTigrusha <DTigrusha@users.noreply.github.com>
2023-05-23 15:20:57 +0300

    KS-12-1 delete (), change isRainy type

--- src/main/kotlin/kotlinsprint/lesson_12/task_1/Task_1.kt
+++ src/main/kotlin/kotlinsprint/lesson_12/task_1/Task_1.kt
@@ -1,15 +1,15 @@
 package kotlinsprint.lesson_12.task_1
 
-class Weather() {
+class Weather {
     var dayTemperature = 20
     var nightTemperature = 10
-    var isRainy = "да"
+    var isRainy = true
     var atmosphericPressure = 755
 
     fun printWeatherInfo() {
         println(
             "Информация о погоде:\nдневная температура - $dayTemperature,\nночная температура - $nightTemperature," +
-                    "\nшел ли дождь - $isRainy,\nатмосферное давление - $atmosphericPressure"
+                    "\nшел ли дождь - ${if (isRainy) "да" else "нет"},\nатмосферное давление - $atmosphericPressure"
         )
     }
 }
@@ -19,7 +19,7 @@ fun main() {
     val weather1 = Weather()
     weather1.dayTemperature = 25
     weather1.nightTemperature = 20
-    weather1.isRainy = "нет"
+    weather1.isRainy = false
     weather1.atmosphericPressure = 765
     weather1.printWeatherInfo()
 
@@ -28,7 +28,7 @@ fun main() {
     val weather2 = Weather()
     weather2.dayTemperature = 10
     weather2.nightTemperature = 3
-    weather2.isRainy = "да"
+    weather2.isRainy = true
     weather2.atmosphericPressure = 750
     weather2.printWeatherInfo()
 
