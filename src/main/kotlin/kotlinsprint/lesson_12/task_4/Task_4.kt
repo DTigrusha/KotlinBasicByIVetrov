e75ca3e08977ab5f87a2bc56ce395d15132ae16d
DTigrusha <DTigrusha@users.noreply.github.com>
2023-05-24 09:46:15 +0300

    KS-12-4 change isRainy type

--- src/main/kotlin/kotlinsprint/lesson_12/task_4/Task_4.kt
+++ src/main/kotlin/kotlinsprint/lesson_12/task_4/Task_4.kt
@@ -3,29 +3,29 @@ package kotlinsprint.lesson_12.task_4
 class Day(
     val dayTemperature: Int,
     val nightTemperature: Int,
-    val isRainy: String = "да",
+    val isRainy: Boolean = true,
     val atmosphericPressure: Int,
 ) {
 
     init {
         println(
             "Информация о погоде:\nдневная температура - $dayTemperature,\nночная температура - $nightTemperature," +
-                    "\nшел ли дождь - $isRainy,\nатмосферное давление - $atmosphericPressure"
+                    "\nшел ли дождь - ${if(isRainy) "да" else "нет"},\nатмосферное давление - $atmosphericPressure"
         )
     }
 }
 
 fun main() {
 
-    val day1 = Day(25, 20, "нет", 765)
+    val day1 = Day(25, 20, false, 765)
 
     println()
 
-    val day2 = Day(10, 3, "да", 750)
+    val day2 = Day(10, 3, true, 750)
 
     println()
 
-    val day3 = Day(35, 33, "нет", 765)
+    val day3 = Day(35, 33, false, 765)
 
 }
 
