b9c00da1f1b832ac86d0047f7692bae553c9427e
DTigrusha <DTigrusha@users.noreply.github.com>
2023-05-29 12:09:45 +0300

    KS-17-3 get name of directory and number of files in it

---
+++ src/main/kotlin/kotlinsprint/lesson_17/task_3/Task_3.kt
@@ -0,0 +1,21 @@
+package kotlinsprint.lesson_17.task_3
+
+class Directory(var secretly: Boolean) {
+    val name: String = "Рабочие документы"
+        get() = if (secretly) "Скрытая папка, количество файлов- $numberOfFiles" else "$field," +
+                "количество файлов - $numberOfFiles"
+    var numberOfFiles: Int = 27
+        get() = if (secretly) 0 else field
+
+}
+
+fun main() {
+
+    val directory = Directory(true)
+
+    println(directory.name)
+
+    directory.secretly = false
+
+    println(directory.name)
+}
