7a2d533f42382f325b613ea2d66037c36adb55d2
DTigrusha <DTigrusha@users.noreply.github.com>
2023-05-29 11:04:27 +0300

    KS-11-5 add changes to functions of Forum class

--- src/main/kotlin/kotlinsprint/lesson_11/task_5/Task_5.kt
+++ src/main/kotlin/kotlinsprint/lesson_11/task_5/Task_5.kt
@@ -1,6 +1,6 @@
 package kotlinsprint.lesson_11.task_5
 
-data class Message(val id: Int, val message: String)
+data class Message(val userId: Int, val message: String)
 
 class User(
     val id: Int,
@@ -12,22 +12,32 @@ class User(
 
 class Forum {
     var lastUserId = 0
+    val listOfUsers = mutableListOf<User>()
+    val listOfMessages = mutableListOf<Message>()
 
     fun newUser(login: String, password: String, email: String, bio: String = ""): User {
-        return User(id = ++lastUserId, login, password, email, bio)
+        listOfUsers.add(User(id = ++lastUserId, login, password, email, bio))
+
+        return User(id = lastUserId, login, password, email, bio)
     }
 
-    fun newMessage(id: Int, message: String): Message {
-        return Message(id, message)
+    fun newMessage(userId: Int, message: String): Message {
+        listOfMessages.add(Message(userId, message))
+
+        return Message(userId, message)
     }
 
-    fun printThread(login: String, message: String) {
-        println("$login: $message")
+    fun printThread() {
+        for (j in listOfMessages) {
+            val i = listOfUsers[j.userId - 1]
+            println("${i.login}: ${j.message}")
+        }
     }
 
 }
 
 fun main() {
+
     val newForum = Forum()
     val newUser1 = newForum.newUser("морковь", "qwerty", "qwerty@mail.ru")
     val newUser2 = newForum.newUser("свекла", "ytrewq", "ytrewq@mail.ru")
@@ -38,9 +48,6 @@ fun main() {
         newForum.newMessage(newUser1.id, "Подскажите, где я могу ознакомится с правилами форума.")
     val secondMessageOfNewUser2 = newForum.newMessage(newUser2.id, "Перейдите по ссылке www.forum/rules!")
 
-    newForum.printThread(newUser1.login, firstMessageOfNewUser1.message)
-    newForum.printThread(newUser2.login, firstMessageOfNewUser2.message)
-    newForum.printThread(newUser1.login, secondMessageOfNewUser1.message)
-    newForum.printThread(newUser2.login, secondMessageOfNewUser2.message)
+    newForum.printThread()
 
 }
