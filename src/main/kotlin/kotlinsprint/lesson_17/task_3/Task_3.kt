dcfbd28138bd3b95f51c690f22705a52034d6294
DTigrusha <DTigrusha@users.noreply.github.com>
2023-05-30 10:50:08 +0300

    KS-17-3 run else to another line

--- src/main/kotlin/kotlinsprint/lesson_17/task_3/Task_3.kt
+++ src/main/kotlin/kotlinsprint/lesson_17/task_3/Task_3.kt
@@ -2,8 +2,8 @@ package kotlinsprint.lesson_17.task_3
 
 class Directory(var secretly: Boolean) {
     val name: String = "Рабочие документы"
-        get() = if (secretly) "Скрытая папка, количество файлов- $numberOfFiles" else "$field," +
-                "количество файлов - $numberOfFiles"
+        get() = if (secretly) "Скрытая папка, количество файлов- $numberOfFiles"
+        else "$field, количество файлов - $numberOfFiles"
     var numberOfFiles: Int = 27
         get() = if (secretly) 0 else field
 
