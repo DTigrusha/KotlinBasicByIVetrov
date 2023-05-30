8fb75033e6f44c7805436ff8e7ea9f0492346124
DTigrusha <DTigrusha@users.noreply.github.com>
2023-05-30 10:04:10 +0300

    KS-18-1 print orders info

---
+++ src/main/kotlin/kotlinsprint/lesson_18/task_1/Task_1.kt
@@ -0,0 +1,30 @@
+package kotlinsprint.lesson_18.task_1
+
+class Order {
+
+    fun printOrderInfo(
+        number: Int,
+        goods: String,
+    ) {
+        println("Заказан товар: \"$goods\".")
+    }
+
+    fun printOrderInfo(
+        number: Int,
+        goods: MutableList<String>,
+    ) {
+        println("Заказаны следующие товары: \"${goods.joinToString()}\".")
+    }
+
+}
+
+fun main() {
+
+    val order1 = Order()
+    val order2 = Order()
+
+    order1.printOrderInfo(1, "телевизор")
+    println()
+    order2.printOrderInfo(2, mutableListOf("миксер", "кофемолка", "кофеварка", "вспениватель"))
+
+}
