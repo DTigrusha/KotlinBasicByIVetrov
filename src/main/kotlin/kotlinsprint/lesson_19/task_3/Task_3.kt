08c0dd5774336e3399f1dc3a758427243a501766
DTigrusha <DTigrusha@users.noreply.github.com>
2023-05-31 15:09:33 +0300

    KS-19-3 use variants of TODO() function

---
+++ src/main/kotlin/kotlinsprint/lesson_19/task_3/Task_3.kt
@@ -0,0 +1,21 @@
+package kotlinsprint.lesson_19.task_3
+
+class Spacecraft {
+    fun takeOff() {
+        // TODO необходимо предусмотреть реальзацию метода
+    }
+
+    fun land() {
+        TODO()
+    }
+
+    fun shoot() {}
+}
+
+fun main() {
+    val spacecraft = Spacecraft()
+
+    spacecraft.takeOff()
+    spacecraft.land()
+
+}
