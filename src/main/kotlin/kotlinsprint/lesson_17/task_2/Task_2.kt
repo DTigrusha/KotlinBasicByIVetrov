d18678b6c783c2dc1095b1dd4aae348cc444bf79
DTigrusha <DTigrusha@users.noreply.github.com>
2023-05-31 12:18:29 +0300

    KS-17-2 transfer ship name to class

--- src/main/kotlin/kotlinsprint/lesson_17/task_2/Task_2.kt
+++ src/main/kotlin/kotlinsprint/lesson_17/task_2/Task_2.kt
@@ -3,22 +3,25 @@ package kotlinsprint.lesson_17.task_2
 class Ship(
     val averageSpeed: Int,
     val homePort: String,
+    name: String,
 ) {
-    var name = "Star"
-        get() = "Star"
+    var name: String = name
         set(value) {
-            field = value
-            if (value != name)
+            if (value != name) {
                 println("Название корабля изменить нельзя!")
+            }
+            field = name
         }
 }
 
 fun main() {
 
     val ship = Ship(
+        name = "Star",
         averageSpeed = 400,
         homePort = "England",
     )
 
     ship.name = "Planet"
+
 }
