3f3098b6446f8a766648a30fdd2efab1b3d02a8e
DTigrusha <DTigrusha@users.noreply.github.com>
2023-05-31 10:01:41 +0300

    KS-18-2 create abstract function and function of throwing die, change class Dice modifier

--- src/main/kotlin/kotlinsprint/lesson_18/task_2/Task_2.kt
+++ src/main/kotlin/kotlinsprint/lesson_18/task_2/Task_2.kt
@@ -1,17 +1,20 @@
 package kotlinsprint.lesson_18.task_2
 
-open class Dice(
+abstract class Dice(
     val numberOfSides: Int,
 ) {
 
-    open fun printNumber() {}
+    abstract fun printNumber()
+
+    fun throwDie(): Int = (1..numberOfSides).random()
+
 }
 
 class FourSidesDie(
     numberOfSides: Int = 4,
 ) : Dice(numberOfSides) {
     override fun printNumber() {
-        println("На кубике с количеством граней: $numberOfSides выпало число: ${(1..numberOfSides).random()}")
+        println("На кубике с количеством граней: $numberOfSides выпало число: ${throwDie()}")
     }
 }
 
@@ -19,7 +22,7 @@ class SixSidesDie(
     numberOfSides: Int = 6,
 ) : Dice(numberOfSides) {
     override fun printNumber() {
-        println("На кубике с количеством граней: $numberOfSides выпало число: ${(1..numberOfSides).random()}")
+        println("На кубике с количеством граней: $numberOfSides выпало число: ${throwDie()}")
     }
 }
 
@@ -27,7 +30,7 @@ class EightSidesDie(
     numberOfSides: Int = 8,
 ) : Dice(numberOfSides) {
     override fun printNumber() {
-        println("На кубике с количеством граней: $numberOfSides выпало число: ${(1..numberOfSides).random()}")
+        println("На кубике с количеством граней: $numberOfSides выпало число: ${throwDie()}")
     }
 }
 
